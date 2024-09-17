149824f8cdf705f349ec2928c46255e0affa7f9c
vivmitra <vivmitra@in.ibm.com>
2024-09-18 01:35:24 +0530

    Add files via upload

---
+++ Rest_Assured/Project/PactProject.java
@@ -0,0 +1,41 @@
+package pactproject;
+
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Provider("UserProvider")
+@PactFolder("target/pacts")
+public class PactProject {
+
+
+    @BeforeEach
+    public void setUp(PactVerificationContext context) {
+        // Set target for provider to send request to
+        context.setTarget(new HttpTestTarget("localhost", 8585));
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    public void provderTest(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+
+    // State to send the call to consumer
+    @State("Post Request")
+    public void state1() {
+
+    }
+
+
+
+
+
+}
